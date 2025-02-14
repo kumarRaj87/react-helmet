008b3649f40f5e57303b5a02aa1c2df9c4eaa7f6
KumarRaj21 <konnakumarraja097@gmail.com>
2025-02-14 15:17:33 +0530

    modified default image

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,12 +3,12 @@ import Meta from "./Meta";
 
 const About = () => (
   <div>
-    {/* <Meta 
+    <Meta 
       title="About Us | My React App"
       description="Learn more about us on this page."
       image="https://react-helmet-one.vercel.app/about-image.png"
       url="https://react-helmet-one.vercel.app/about"
-    /> */}
+    />
     <h1>About Page</h1>
   </div>
 );
